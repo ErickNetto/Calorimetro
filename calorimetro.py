d249ef68efe1fb2d87e3d1884aacb2130a15aa65
ErickNetto <102562548+ErickNetto@users.noreply.github.com>
2023-11-16 10:55:13 -0300

    acho que agr ta certo
    espero que esteja certo

--- calorimetro.py
+++ calorimetro.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 m1 = 100;
 c1 = 1;
-tia = int(input("Qual a temperatura inicial da água? (Celsius)\r\n"));
-tim = int(input("Qual a temperatura inicial da moeda? (Celsius)\r\n"));
-tfa = int(input("Qual a temperatura final da água? (Celsius)\r\n "));
-tfm = int(input("Qual a temperatura final da moeda? (Celsius)\r\n"));
-m2 = 4.8;
+tia = float(input("Qual a temperatura inicial da água? (Celsius)\r\n"));
+tim = float(input("Qual a temperatura inicial da moeda? (Celsius)\r\n"));
+tfa = float(input("Qual a temperatura final da água? (Celsius)\r\n "));
+tfm = float(input("Qual a temperatura final da moeda? (Celsius)\r\n"));
+m2 = 4.78;
 
-
-
-c2 = -((m1*c1*(tfa - tia))/(m2*(tfm - tim)));
-print("O calor específico da moeda é:", c2);
+c2 = (-((m1*c1*(tfa - tia))/(m2*(tfm - tim))));
+print("O calor específico da moeda é:", c2, "cal/g.");
